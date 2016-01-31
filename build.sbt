1ea7dcc65980f85a4e09c50cc46239a1cb449733
aleksander ihnatowicz <olek@vintom.com>
2016-01-31 23:10:07 +0100

    adding first test

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "PrimeFactorsKata"
 version := "1.0"
 
 libraryDependencies += "junit" % "junit" % "4.10" % Test
+
+libraryDependencies += "org.assertj" % "assertj-core" % "3.3.0" % Test
+
--- src/test/java/primeFactors/PrimeFactorsTest.java
+++ src/test/java/primeFactors/PrimeFactorsTest.java
@@ -1,7 +1,20 @@
 package primeFactors;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeFactorsTest {
 
+    @Test
+    public void testOne() {
+        assertThat(PrimeFactors.generate(1)).isEqualTo(list());
+    }
+
+    private List<Integer> list() {
+        return new ArrayList<>();
+    }
 }
